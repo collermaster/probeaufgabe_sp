01778a9345880c1989108b3afdd8795fba1237b4
lucasmacbookm3 <lucasnielsen@werbewolke.de>
2025-05-24 17:04:29 +0200

    sort results, add comis

--- app/Console/Commands/AnalyzeLog.php
+++ app/Console/Commands/AnalyzeLog.php
@@ -26,41 +26,59 @@ class AnalyzeLog extends Command
     public function handle()
     {
 
-//        https://www.uptimia.com/questions/how-to-read-a-large-file-line-by-line-in-php
+        // ich hab die .log Datei unter storage/analyzer/updatev12-access-pseudonymized.log gespeichert
+        // mit fopen die Logs öffnen und dann mit fget Zeile für Zeile auslesen und bearbeiten,
+        // damit nicht die komplette Datei (>500mb) in den Speicher eingelesen wird
+        // https://www.uptimia.com/questions/how-to-read-a-large-file-line-by-line-in-php
 
         $handle = fopen("storage/analyzer/updatev12-access-pseudonymized.log", "r");
-        $count = 0;
+        global $serialCount;
+        global $serial;
+        global $serialToMacs;
+        global $deviceMAC;      //variablen global zur verfügung gestellt, um nach der while schleife damit zu arbeiten
 
-        while (($line = fgets($handle)) !== false && $count <= 10) {
-//            $parts = explode(' ', $line);
-//
-//            foreach ($parts as $part) {
-//                if (str_starts_with($part, 'serial=')) {
-//                    $serial = substr($part, 7);
-//                    echo $serial;
-//                }
-//
-//                if (str_starts_with($part, 'specs=')) {
-//                    $specs = substr($part, 6);
-//                    echo $specs;
-//                }
-//
-//            }
+        while (($line = fgets($handle)) !== false) {
 
-            preg_match('/serial=([A-F0-9]+)/', $line, $serialMatch);
-            $serial = $serialMatch[1] ?? null;
+            // mit Regex die Serial number und specs extrahieren, erst hab ich das mit explored versucht (weil schneller als Regex) und dann bei jedem Leerzeichen getrennt,
+            // hab das aber nicht wirklich zum Laufen bekommen
 
+            if (preg_match('/serial=([A-Z0-9]+)/', $line, $match)) {
+                $serial = $match[1];
+                if (!isset($serialCount[$serial])) {
+                    $serialCount[$serial] = 0;
+                }
+                $serialCount[$serial]++;
+            }
 
-            preg_match('/specs=([A-Za-z0-9+\/=]+)/', $line, $specsMatch);
-            $data = json_decode(gzdecode(base64_decode($specsMatch[1])), true);
-            $deviceMAC = $data['mac'];
-            var_dump($deviceMAC);
+            // hier war ich erst etwas überfordert da ich nicht gesehen hatte das base65 und GZIP in der Anweisung gegeben war danach hatte ich probleme mit gzdecode,
+            // habe da immer data error bekommen, mit dem @ werden warnings ignoriert und damit hat das danach einfach funktioniert, ich wei jetzt ehrlicherweise nicht wieso
+            // bzw was der Fehler wart
 
-
-
-            $count++;
+            if (preg_match('/specs=([a-zA-Z0-9\/+]+)/', $line, $match)) {
+                $data = json_decode(@gzdecode(base64_decode($match[1])), true);
+                if (isset($data['mac'])) {
+                    $deviceMAC = $data['mac'];
+                }
+                $serialToMacs[$serial][$deviceMAC] = true;
+            }
         }
         fclose($handle);
 
+        // erst wusste ich nicht wie ich hier weiter machen soll,
+        // dachte erst das ich die Serial und die Specs als objekt erzeuge um dann Abhängigkeiten zu erzeugen aber ein ähnliches problem hatte ich schon mal bei einem Project von uns
+        // da hab ich aber eine custom Collection erstellt und darüber hab ich dann die collect() gefunden, sehr geiles Ding um damit die top 10 herauszuholen
+
+        $sortedSerialCount = collect($serialCount)
+            ->sortDesc()
+            ->take(10);
+//        var_dump($sortedSerialCount);
+        $sortedSerialToMacs = collect($serialToMacs)
+            ->sortDesc()
+            ->take(10);
+//        var_dump($sortedSerialToMacs);
+
+
+        //hier würde ich jetzt die aufforderung einauen die die information in einer PDF speichert, dazu würde ich dann eine view.blade.php erstellen die den Content des PDFs erzeugt
     }
+
 }
